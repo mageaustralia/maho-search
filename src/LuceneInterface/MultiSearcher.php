36b14976ef3ce119b931925034b8811698d3a7ed
Matthew Campbell <matthewcampbell@mageaustralia.com.au>
2026-03-27 14:06:06 +1100

    Restore original Zend copyright in library file headers
    BSD 3-Clause requires retaining the original copyright notice.
    All library files now carry both the original Zend Technologies
    copyright and the Mage Australia copyright.

--- src/LuceneInterface/MultiSearcher.php
+++ src/LuceneInterface/MultiSearcher.php
@@ -16,6 +16,9 @@ namespace Maho\Search\Lucene\LuceneInterface;
  * obtain it through the world-wide-web, please send an email
  * to license@zend.com so we can send you a copy immediately.
  *
+ * @copyright  Copyright (c) 2005-2015 Zend Technologies USA Inc. (https://www.zend.com)
+ * @copyright  Copyright (c) 2026 Mage Australia Pty Ltd
+ * @license    https://opensource.org/licenses/BSD-3-Clause  BSD 3-Clause License
  * @category   Maho
  * @package    Maho_Search_Lucene
  * @version    $Id: MultiSearcher.php 24593 2012-01-05 20:35:02Z matthew $
--- src/Storage/Directory/Filesystem.php
+++ src/Storage/Directory/Filesystem.php
@@ -16,6 +16,9 @@ namespace Maho\Search\Lucene\Storage\Directory;
  * obtain it through the world-wide-web, please send an email
  * to license@zend.com so we can send you a copy immediately.
  *
+ * @copyright  Copyright (c) 2005-2015 Zend Technologies USA Inc. (https://www.zend.com)
+ * @copyright  Copyright (c) 2026 Mage Australia Pty Ltd
+ * @license    https://opensource.org/licenses/BSD-3-Clause  BSD 3-Clause License
  * @category   Maho
  * @package    Maho_Search_Lucene
  * @subpackage Storage
